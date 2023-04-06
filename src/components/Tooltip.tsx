32ea171feef27e3a35f136ef39b745dbbb9e3e77
mikowl <michael@mikowl.com>
2023-04-06 16:37:08 -0700

    Add tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import useUniqueId from "../utils/useUniqueId";
+
+type TooltipPros = {
+	content: string;
+	children: React.ReactElement;
+};
+
+const Tooltip: React.FC<TooltipPros> = ({ content, children }) => {
+	const [show, setShow] = useState(false);
+	const tooltipId = useUniqueId("tooltip");
+
+	const handleMouseEnter = () => {
+		setShow(true);
+	};
+
+	const handleMouseLeave = () => {
+		setShow(false);
+	};
+
+	const handleFocus = () => {
+		setShow(true);
+	};
+
+	const handleBlur = () => {
+		setShow(false);
+	};
+
+	return (
+		<span
+			className="tooltip-container"
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			onFocus={handleFocus}
+			onBlur={handleBlur}
+			tabIndex={0}
+			aria-describedby={tooltipId}
+		>
+			{children}
+			{show && (
+				<span className="tooltip-content" id={tooltipId} role="tooltip">
+					{content}
+				</span>
+			)}
+		</span>
+	);
+};
+
+export default Tooltip;
